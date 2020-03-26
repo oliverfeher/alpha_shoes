36b997388bf383e918967ace800d6629c436f787
oliverfeher <oliver92.feher@gmail.com>
2020-03-26 18:23:26 -0400

    Add helper methods for login, allso add auth_req method
    - Auth_req method to check if the user is logged_in, else redirect
    - current_user helper method to use instance variable in actionviews
    - logged_in? to check if the user is logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+
+    # Using it for before_action: auth_req... user only able to see the page if user is logged_in
+    def authentication_required
+        if !logged_in?
+            redirect_to login_path
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id])
+    end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:user][:email])
+        session[:user_id] = user.id
     end
-    
+
 end
