104f99f1958369a5cf2f44320938a3bd7f430d2a
oliverfeher <oliver92.feher@gmail.com>
2020-03-26 14:28:58 -0400

    Create sessions controller, and related routes
    - Create new#sessions
    - Create create#sessions
    - User log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "/login" => "sessions#new", as: :login
+  post "/sessions" => "sessions#create"
+
+
   resources :users, only: [:new, :create]
 end
