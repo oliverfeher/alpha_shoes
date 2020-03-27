6f31f0ceb497da3c6c77fcdd205ccfad1c371106
oliverfeher <oliver92.feher@gmail.com>
2020-03-26 23:28:54 -0400

    Create logout function
    - Route session#destroy to /logout
    - Create logout button on index
    - Clears session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,20 +9,30 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to root_path
         else
+
+            # if can't find any user with the given e-mail / or e-mail field was blank
             if user.nil?
                 flash[:notice] = "This e-mail doesn't exist in our database!"
                 redirect_to action: "new"
+
+            # if password blank display that it cannot be blank
             elsif
                 params[:password].blank?
                 flash[:notice] = "Password can't be blank!"
                 redirect_to action: "new"
+
+            # if given password doesn't authenticate then display Inccorect PWD
             elsif
                 !user.authenticate(params[:password])
                 flash[:notice] = "Incorrect password!"
                 redirect_to action: "new"
             end
         end
-    
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
     end
 
 end
