d2a66f1878d75e1d27ee659d5cfcf32b967b92a0
oliverfeher <oliver92.feher@gmail.com>
2020-03-26 21:36:40 -0400

    Fix helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+    #Allows to use current_user method in the actionviews
+    helper_merhod: :current_user
+
 
     # Using it for before_action: auth_req... user only able to see the page if user is logged_in
     def authentication_required
@@ -13,7 +16,9 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        @current_user ||= User.find(session[:user_id])
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+
+
 
 end
