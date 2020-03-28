f7c74ce846cf757f182aa171fd81ad122c8f9690
oliverfeher <oliver92.feher@gmail.com>
2020-03-27 23:06:27 -0400

    Link stylesheet to login form, fix footer positioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/login" => "sessions#new"
-  get "/logout" => "sessions#destroy"
+  get "/users/login" => "sessions#new"
+  get "/users/logout" => "sessions#destroy"
   post "/sessions" => "sessions#create"
   root 'home#index'
 
