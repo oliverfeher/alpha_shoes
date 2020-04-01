98f12b136f880f3608a558b45472cff6fab1bd02
oliverfeher <oliver92.feher@gmail.com>
2020-03-31 20:41:50 -0400

    Build Reviews
    - Create new action view for reviews ctrl
    - Create validation for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
     has_many :shoes_reviews
     has_many :shoes, through: :shoes_reviews, dependent: :nullify
     belongs_to :user
+
+    validates :rating, presence: true
+    validates :content, presence: true
 end
