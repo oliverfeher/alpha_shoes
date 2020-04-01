7e4e47054a1247dd36762aa10769666b718b3ab4
oliverfeher <oliver92.feher@gmail.com>
2020-04-01 04:03:24 -0400

    Finish avg rating logic and style

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,13 @@ class ReviewsController < ApplicationController
         @shoe = Shoe.find_by(id: params[:shoe_id])
         review = @shoe.reviews.build(review_params)
         review.save
+        redirect_to shoe_reviews_path(@shoe)
+    end
+    
+    def show
+        @shoe = Shoe.find_by(id: params[:shoe_id])
+        avg_arr = @shoe.reviews.map { |x| x.rating }
+        @avg = avg_arr.inject { |sum, el| sum + el }.to_f / avg_arr.size 
     end
     
 
