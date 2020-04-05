fc3074e0903e48e7c938fddb7849904bd0105ee6
oliverfeher <oliver92.feher@gmail.com>
2020-04-05 15:00:23 -0400

    Orders controller fix

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -7,7 +7,7 @@ class DetailsController < ApplicationController
 
     def create
         @detail = Detail.create(user_id: current_user.id, city: params[:city], address: params[:city], state: params[:state], full_name: params[:full_name])
-        redirect_to :root
+        redirect_to "/"
 
     end
 end
