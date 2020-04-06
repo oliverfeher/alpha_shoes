a022620245814d8d25147de73b8a976ca66de220
oliverfeher <oliver92.feher@gmail.com>
2020-04-06 15:16:02 -0400

    Add #details_params to details ctrl
    - Build strong params for detail creation
    - Use form_with helper

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,13 +1,20 @@
 class DetailsController < ApplicationController
     def new
+        @detail = Detail.new
     end
 
     def show
     end
 
     def create
-        @detail = Detail.create(user_id: current_user.id, city: params[:city], address: params[:city], state: params[:state], full_name: params[:full_name])
+        @detail = Detail.create(details_params)
+        binding.pry
         redirect_to "/"
+    end
 
+    private
+
+    def details_params
+        params.require(:detail).permit(:city, :address, :state, :full_name, :user_id)
     end
 end
